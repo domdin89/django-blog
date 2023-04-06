a1d28eefa2ada02d9c18a9ff24b95d3b830ecd4c
Domenico Di Nardo <domenico.dnrd@hotmail.com>
2023-04-06 11:03:19 +0200

    footer and css

--- posts/urls.py
+++ posts/urls.py
@@ -8,6 +8,7 @@ app_name='post'
 urlpatterns = [
     path('blog/', views.blog, name='blog'),
     path('blog/detail/<int:id>', views.post_detail, name='post-detail'),
+    path('blog/new/', views.new, name='new'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # pragma: nocover
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
+from posts.forms import NewPostForm
 from posts.models import Post
 
 
@@ -10,3 +12,15 @@ def blog(request):
 def post_detail(request, id):
     post = Post.objects.get(id=id)
     return render(request, 'posts/post-details.html', {'post': post})
+
+def new(request):
+    if request.method == 'POST':
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = NewPostForm()
+    return render(request, 'posts/add_post.html', {'form': form})
